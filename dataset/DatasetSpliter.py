98d7e23280815c74b2e57acd83f61e1cd7eb37ca
mrlex <1005360764@qq.com>
2024-12-06 16:38:38 +0800

    Add Pathological Split

--- dataset/DatasetSpliter.py
+++ dataset/DatasetSpliter.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 import numpy.random
 import random
 from torch.utils.data import Dataset, DataLoader
@@ -22,6 +23,25 @@ class DatasetSpliter:
         
         return
     
+    def _divide_into_shards(self, l: list, g: int):
+        """
+        divide a list into g groups
+        """
+        num_elems = len(l)
+        group_size = int(len(l) / g)
+        # groups that have one more sample than others
+        num_big_groups = num_elems - g * group_size
+        num_small_groups = g - num_big_groups
+        glist = []
+        for i in range(num_small_groups):
+            glist.append(l[group_size * i: group_size * (i + 1)])
+        bi = group_size * num_small_groups
+        group_size += 1
+        for i in range(num_big_groups):
+            glist.append(l[bi + group_size * i:bi + group_size * (i + 1)])
+        return glist
+
+
     def _sample_dirichlet(self, dataset: Dataset, client_list: dict, alpha: int) -> defaultdict(list):
         client_num = len(client_list.keys())
         per_class_list = defaultdict(list)
@@ -45,12 +65,53 @@ class DatasetSpliter:
                 per_client_list[client_id].extend(sampled_list)
                 per_class_list[n] = per_class_list[n][min(len(per_class_list[n]), no_imgs):]
         return per_client_list 
+    
+    def _sample_pathological(self, dataset: Dataset, client_list: dict, classes_per_client_num: int) -> defaultdict(list):
+        client_num = len(client_list.keys())
+        per_class_list = defaultdict(list)
+
+        data_idcs = list(range(len(dataset)))
+        label2index = defaultdict(list)
+        for idx in data_idcs:
+            _, label = dataset[idx]
+            label2index[label].append(idx)
+        
+        sorted_idcs = []
+        for label in label2index:
+            sorted_idcs += label2index[label]
+        
+        shards_num = client_num * classes_per_client_num
+        shards = self._divide_into_shards(sorted_idcs, shards_num)
+        np.random.shuffle(shards)
+        tasks_shards = self._divide_into_shards(shards, client_num)
+        for ind, (client_id, _) in enumerate(client_list.items()):
+            for shard in tasks_shards[ind]:
+                per_class_list[client_id] += shard
+        return per_class_list
         
     def dirichlet_split(self, dataset: Dataset, client_list: dict, batch_size: int = 32, alpha: int = 1) -> dict[DataLoader]:
         #get each client samples
         split_list = self._sample_dirichlet(dataset = dataset, 
                                             client_list = client_list,
                                             alpha = alpha)
+        dataloaders = defaultdict(DataLoader)
+        
+        #construct dataloader
+        for ind, (client_id, _) in enumerate(client_list.items()):
+            indices = split_list[client_id]
+            this_dataloader = DataLoader(dataset    = dataset,
+                                         batch_size = batch_size,
+                                         sampler    = SubsetRandomSampler(indices),
+                                         num_workers= 4)
+            dataloaders[client_id] = this_dataloader 
+        
+        return dataloaders
+
+    def pathological_split(self, dataset: Dataset, client_list: dict, batch_size: int = 32, classes_per_client_num: int = 2) -> dict[DataLoader]:
+        #get each client samples
+        split_list = self._sample_pathological(dataset = dataset, 
+                                            client_list = client_list,
+                                            classes_per_client_num = classes_per_client_num)
         dataloaders = defaultdict(DataLoader)
         
         #construct dataloader
